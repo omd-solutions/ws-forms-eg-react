8ed9cc4f3053551061c02f28a9aef17223ae5faf
oliverdennis <oliver.dennis@omdsolutions.co.uk>
2020-12-17 15:05:31 +0000

    Testing password field

--- src/main/java/com/omd/ws/forms/eg/EmployeeEntity.java
+++ src/main/java/com/omd/ws/forms/eg/EmployeeEntity.java
@@ -24,6 +24,10 @@ public class EmployeeEntity {
     @Text(validationRegex = "0[0-9]{10}", validationMessage = "Must be 11 digits and start with a 0")
     private String phoneNumber;
 
+    @FormField(columns = 6, panel = "personalDetails")
+    @Text(masked = true, validationRegex = "^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!#$%&? \"]).*$", validationMessage = "Must be at least 8 characters with at least 1 number, capital and special char")
+    private String password;
+
     @FormField(columns = 6, panel = "location")
     @Select(displayField = "name", valueProvider = CountryValueProvider.class)
     private Country country;
@@ -93,6 +97,14 @@ public class EmployeeEntity {
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
     public Country getCountry() {
